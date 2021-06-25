c47d1796d9a855363588783b176e2a35a5d2bade
Brent Picasso <brent@autosportlabs.com>
2021-06-24 23:30:52 -0700

    disable pkgresources

--- sagecipher/__init__.py
+++ sagecipher/__init__.py
@@ -1,7 +1,8 @@
-import pkg_resources
+#import pkg_resources
 
 from sagecipher.cipher import Cipher, SshAgentKeyError
 
-__version__ = pkg_resources.get_distribution("sagecipher").version
+# pkg_resources disabled to work with pyinstaller
+__version__ = "0.7.5" #pkg_resources.get_distribution("sagecipher").version
 
 __all__ = [Cipher, SshAgentKeyError, __version__]
